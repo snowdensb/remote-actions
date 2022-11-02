f0d93a84ccdeaaf5a64503e8f745d3ff9d1d3062
Abhishek Singh <abhishek.singh3@fiserv.com>
2022-11-02 13:32:00 -0400

    Config check

--- scripts/config-validator.js
+++ scripts/config-validator.js
@@ -6,9 +6,11 @@ const SwaggerParser = require('@apidevtools/swagger-parser');
 const args = process.argv.slice(2); 
 const folder = args?.[0]+"/config"; 
 const {errorMessage  , printMessage} = require('./utils/tools')
-const validator_name = 'DED VALIDATOR'; 
+const ded_validator  = 'DED VALIDATOR'; 
+const tenant_config_validator = 'TENANT CONFIG VALIDATOR'; 
+let check = true;
 const validateDir = async (dir) => {
-let check = false;
+
 
   fs.readdir(dir, { withFileTypes: true }, (err, files) => {
     files?.forEach(async file => {
@@ -17,12 +19,10 @@ let check = false;
         try {
           const fileName = `${dir}/${file.name}`;
           const content = fs.readFileSync(fileName, 'utf8');
-          const apiJson = yaml.load(content);
-          
-          //linkcheck(apiJson);
-
+          const apiJson = yaml.load(content);  
         } catch (e) {
-          errorMessage(validator_name  ,e?.message);
+          errorMessage(ded_validator  ,e?.message);
+          check = false;
         }
       } 
 
@@ -32,44 +32,53 @@ let check = false;
           const fileName = `${dir}/${file.name}`;
           const content = fs.readFileSync(fileName, 'utf8'); 
           JSON.parse(content)
-
+          
         }catch (e){
-          errorMessage(validator_name  ,e?.message);
+          errorMessage(tenant_config_validator  ,e?.message);
+          check = false;
         } 
       }
       
     });
+
+    if (check){
+      printMessage(`${tenant_config_validator} and ${ded_validator}: PASSED`);
+      }
+    
   });
 };
 
-function linkcheck(jsonArray){
+// function linkcheck(jsonArray){
 
-    jsonArray.forEach((item) => { 
-        for (res of item?.sections){  
+//     jsonArray.forEach((item) => { 
+//         for (res of item?.sections){  
             
-            if (res instanceof Object){
-              //console.log(res);
-                if (res?.link){
-                  console.log(res?.link);
-                }
-                for (res1 of res?.sections){
-                  console.log(res1?.link);
-                  if (res1?.sections instanceof Object){
-                    linkcheck(res1?.sections)
-                  }
+//             if (res instanceof Object){
+//               //console.log(res);
+//                 if (res?.link){
+//                   console.log(res?.link);
+//                 }
+//                 for (res1 of res?.sections){
+//                   console.log(res1?.link);
+//                   if (res1?.sections instanceof Object){
+//                     linkcheck(res1?.sections)
+//                   }
                  
-                }
-            }
+//                 }
+//             }
           
-        } 
-      }); 
-    return true;
-}
+//         } 
+//       }); 
+//     return true;
+// }
 
 try {
   printMessage(`External Dir ---->>> ${folder}`);   
   if ( args?.length > 0){ 
   validateDir(folder);
+  if (check){
+    printMessage(`External Dir ---->>> ${folder}`); 
+  }
  }else{
   errorMessage('YAML VALIDATOR'  ,'No Path for reference dir. defined');
  }
