de6ed0f086517e92e4c3bb12bcd864e182843a41
Abhishek Singh <abhishek.singh3@fiserv.com>
2022-12-01 14:46:03 -0500

    fix

--- scripts/utils/tools.js
+++ scripts/utils/tools.js
@@ -1,33 +1,32 @@
 const processArgs = (args = []) => {
-    const argsAndValues = {};
-    for (let i = 0; i < args.length; i++) {
-      const arg = args[i];
-      if (/^--.+/.test(arg)) {
-        const key = arg.match(/^--(.+)/)[1];
-        const next = args[i + 1];
-        if (/^--.+/.test(next)) {
-          argsAndValues[key] = false;
-          continue;
-        }
-        argsAndValues[key] = next;
-        i++;
+  const argsAndValues = {};
+  for (let i = 0; i < args.length; i++) {
+    const arg = args[i];
+    if (/^--.+/.test(arg)) {
+      const key = arg.match(/^--(.+)/)[1];
+      const next = args[i + 1];
+      if (/^--.+/.test(next)) {
+        argsAndValues[key] = false;
+        continue;
       }
+      argsAndValues[key] = next;
+      i++;
     }
-    return argsAndValues;
-  };
-
-  const errorMessage = (type , message) => {
-    console.error(`\x1b[31m -------------------------${type} FAILED -------------------------- \x1b[5m`);
-    console.error(`\x1b[33m ${message} \x1b[1m`);
-  };
-  
-  const printMessage = (message) => { 
-    //console.dir(message , { 'colors': true, "depth": null });
-    console.log(`\x1b[32m ${message} \x1b[0m`);
   }
+  return argsAndValues;
+};
 
-  module.exports = {
-    processArgs,
-    errorMessage,
-    printMessage,
-  };
+const errorMessage = (type , message) => {
+  console.dir(`-------------------------${type} FAILED --------------------------`, { 'colors': true, "depth": null });
+  console.dir(message , { 'colors': true, "depth": null })
+};
+
+const printMessage = (message) => { 
+  console.dir(message , { 'colors': true, "depth": null })
+}
+
+module.exports = {
+  processArgs,
+  errorMessage,
+  printMessage,
+};
